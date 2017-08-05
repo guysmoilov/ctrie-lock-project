23d687c2365ed902b14f65ed15edd63d1fde298c
Guy Smoilovsky <guy.smoilov@clarisite.com>
2017-08-05 18:25:06 +0300

    Replaced RDCSS_ROOT stuff with locks

--- src/main/scala/ctrielock/ConcurrentTrie.scala
+++ src/main/scala/ctrielock/ConcurrentTrie.scala
@@ -1,193 +1,116 @@
 package ctrielock
 
-import java.util.concurrent.atomic._
-
 import scala.annotation.tailrec
-import scala.collection.Map
-import scala.collection.mutable.ConcurrentMap
+import scala.collection.{Map, mutable, immutable}
 
+class ConcurrentTrie[K, V] private (rt: INode[K,V], isReadOnly : Boolean)
+extends mutable.ConcurrentMap[K, V] {
 
+  private val rootLock = new Object
+  @volatile private var root = rt
 
-@Deprecated
-case class RDCSS_Descriptor[K, V](old: INode[K, V], expectedmain: MainNode[K, V], nv: INode[K, V]) {
-  @volatile var committed = false
-}
+  def this(r: INode[K,V]) = this(r, false)
 
-class ConcurrentTrie[K, V] private (r: AnyRef, rtupd: AtomicReferenceFieldUpdater[ConcurrentTrie[K, V], AnyRef])
-extends ConcurrentMap[K, V]
-{
-  import ConcurrentTrie.computeHash
-
-  @Deprecated
-  private val rootupdater = rtupd
-  @volatile var root = r
-  
-  def this() = this(
-    INode.newRootNode,
-    AtomicReferenceFieldUpdater.newUpdater(classOf[ConcurrentTrie[K, V]], classOf[AnyRef], "root")
-  )
+  def this() = this(INode.newRootNode : INode[K,V])
   
   /* internal methods */
 
-  @Deprecated
-  @inline final def CAS_ROOT(ov: AnyRef, nv: AnyRef) = rootupdater.compareAndSet(this, ov, nv)
-
-  @Deprecated
-  @inline final def RDCSS_READ_ROOT(abort: Boolean = false): INode[K, V] = {
-    val r = /*READ*/root
-    r match {
-      case in: INode[K, V] => in
-      case desc: RDCSS_Descriptor[K, V] => RDCSS_Complete(abort)
-    }
+  @inline private def READ_ROOT() = {
+    root
   }
 
-  @Deprecated
-  @tailrec private def RDCSS_Complete(abort: Boolean): INode[K, V] = {
-    val v = /*READ*/root
-    v match {
-      case in: INode[K, V] => in
-      case desc: RDCSS_Descriptor[K, V] =>
-        val RDCSS_Descriptor(ov, exp, nv) = desc
-        if (abort) {
-          if (CAS_ROOT(desc, ov)) ov
-          else RDCSS_Complete(abort)
-        } else {
-          val oldmain = ov.GCAS_READ(this)
-          if (oldmain eq exp) {
-            if (CAS_ROOT(desc, nv)) {
-              desc.committed = true
-              nv
-            } else RDCSS_Complete(abort)
-          } else {
-            if (CAS_ROOT(desc, ov)) ov
-            else RDCSS_Complete(abort)
-          }
-        }
-    }
-  }
-
-  @Deprecated
-  private def RDCSS_ROOT(ov: INode[K, V], expectedmain: MainNode[K, V], nv: INode[K, V]): Boolean = {
-    val desc = RDCSS_Descriptor(ov, expectedmain, nv)
-    if (CAS_ROOT(ov, desc)) {
-      RDCSS_Complete(false)
-      /*READ*/desc.committed
-    } else false
+  @inline private def WRITE_ROOT(newR : INode[K,V]) = {
+    root = newR
   }
   
   @tailrec private def inserthc(k: K, hc: Int, v: V) {
-    val r = RDCSS_READ_ROOT()
+    val r = READ_ROOT()
     if (!r.rec_insert(k, v, hc, 0, null, r.gen, this)) inserthc(k, hc, v)
   }
   
   @tailrec private def insertifhc(k: K, hc: Int, v: V, cond: AnyRef): Option[V] = {
-    val r = RDCSS_READ_ROOT()
-    
+    val r = READ_ROOT()
     val ret = r.rec_insertif(k, v, hc, cond, 0, null, r.gen, this)
     if (ret eq null) insertifhc(k, hc, v, cond)
     else ret
   }
   
   @tailrec private def lookuphc(k: K, hc: Int): AnyRef = {
-    val r = RDCSS_READ_ROOT()
+    val r = READ_ROOT()
     val res = r.rec_lookup(k, hc, 0, null, r.gen, this)
     if (res eq INode.RESTART) lookuphc(k, hc)
     else res
   }
   
   @tailrec private def removehc(k: K, v: V, hc: Int): Option[V] = {
-    val r = RDCSS_READ_ROOT()
+    val r = READ_ROOT()
     val res = r.rec_remove(k, v, hc, 0, null, r.gen, this)
     if (res ne null) res
     else removehc(k, v, hc)
   }
   
-  def string = RDCSS_READ_ROOT().string(0)
+  def string = READ_ROOT().string(0)
   
   /* public methods */
   
-  @inline final def isReadOnly = rootupdater eq null
-  
-  @inline final def nonReadOnly = rootupdater ne null
+  @inline final def nonReadOnly = !isReadOnly
 
-  @Deprecated
-  @tailrec final def snapshot(): ConcurrentTrie[K, V] = {
-    val r = RDCSS_READ_ROOT()
-    val expmain = r.GCAS_READ(this)
-    if (RDCSS_ROOT(r, expmain, r.copyToGen(new Gen, this))) new ConcurrentTrie(r.copyToGen(new Gen, this), rootupdater)
-    else snapshot()
+  final def snapshot(): ConcurrentTrie[K, V] = {
+    doSnapshotSync(false)
   }
 
-  @Deprecated
-  @tailrec final def readOnlySnapshot(): Map[K, V] = {
-    val r = RDCSS_READ_ROOT()
-    val expmain = r.GCAS_READ(this)
-    if (RDCSS_ROOT(r, expmain, r.copyToGen(new Gen, this))) new ConcurrentTrie(r, null)
-    else readOnlySnapshot()
+  final def readOnlySnapshot(): Map[K, V] = {
+    doSnapshotSync(true)
   }
 
-  @Deprecated
-  @tailrec final override def clear() {
-    val r = RDCSS_READ_ROOT()
-    if (!RDCSS_ROOT(r, r.GCAS_READ(this), INode.newRootNode[K, V])) clear()
+  final override def clear() {
+    // I think this should be enough... need to think about it carefully
+    rootLock.synchronized {
+      WRITE_ROOT(INode.newRootNode)
+    }
   }
 
-  final def doSnapshotSync(snapshotRootUpdater: AtomicReferenceFieldUpdater[ConcurrentTrie[K, V], AnyRef]): ConcurrentTrie[K, V] = {
-    this.synchronized {
+  final def doSnapshotSync(readonlySnapshot : Boolean): ConcurrentTrie[K, V] = {
+    rootLock.synchronized {
       // Can safely use currRoot in the rest of this method body, since we locked the CTrie and no one can change the root
       // except the current thread
-      val currRootTmp = root
-      currRootTmp match {
-        case currRoot : INode[K,V] =>
-          currRoot.synchronized {
-            val newRoot = currRoot.copyToGen(new Gen, this)
-            val snapshotRoot = currRoot.copyToGen(new Gen, this)
-            this.root = newRoot
-            return new ConcurrentTrie[K, V](snapshotRoot, snapshotRootUpdater)
-          }
-        case _ => throw new IllegalArgumentException // TODO: Make root always an indoe
+      val currRoot = READ_ROOT()
+      // Also lock on the root itself to prevent a thread modifying its main node while we're copying it.
+      currRoot.synchronized {
+        val newRoot = currRoot.copyToGen(new Gen, this)
+        // If the snapshot we return is destined to be readOnly, we can reuse the current root, since we know the snapshot
+        // won't be mutating it, and mutations on "this" will occur on the new generation root
+        val snapshotRoot = if (readonlySnapshot) currRoot else currRoot.copyToGen(new Gen, this)
+        WRITE_ROOT(newRoot) // Linearization point
+        return new ConcurrentTrie[K, V](snapshotRoot, readonlySnapshot)
       }
     }
   }
 
-  final def snapshotSync(): ConcurrentTrie[K, V] = {
-    doSnapshotSync(rootupdater)
-  }
-
-  final def readOnlySnapshotSync(): Map[K, V] = {
-    doSnapshotSync(null)
-  }
-
-  final def clearSync() = {
-    this.synchronized(
-      root = INode.newRootNode[K,V]
-    )
-  }
-
   final def lookup(k: K): V = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     lookuphc(k, hc).asInstanceOf[V]
   }
   
   final override def apply(k: K): V = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     val res = lookuphc(k, hc)
     if (res eq null) throw new NoSuchElementException
     else res.asInstanceOf[V]
   }
   
   final def get(k: K): Option[V] = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     Option(lookuphc(k, hc)).asInstanceOf[Option[V]]
   }
   
   override def put(key: K, value: V): Option[V] = {
-    val hc = computeHash(key)
+    val hc = ConcurrentTrie.computeHash(key)
     insertifhc(key, hc, value, null)
   }
   
   final override def update(k: K, v: V) {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     inserthc(k, hc, v)
   }
   
@@ -197,7 +120,7 @@ extends ConcurrentMap[K, V]
   }
   
   final override def remove(k: K): Option[V] = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     removehc(k, null.asInstanceOf[V], hc)
   }
   
@@ -207,29 +130,32 @@ extends ConcurrentMap[K, V]
   }
   
   def putIfAbsent(k: K, v: V): Option[V] = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     insertifhc(k, hc, v, INode.KEY_ABSENT)
   }
   
   def remove(k: K, v: V): Boolean = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     removehc(k, v, hc).nonEmpty
   }
   
   def replace(k: K, oldvalue: V, newvalue: V): Boolean = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     insertifhc(k, hc, newvalue, oldvalue.asInstanceOf[AnyRef]).nonEmpty
   }
   
   def replace(k: K, v: V): Option[V] = {
-    val hc = computeHash(k)
+    val hc = ConcurrentTrie.computeHash(k)
     insertifhc(k, hc, v, INode.KEY_PRESENT)
   }
   
   def iterator: Iterator[(K, V)] =
     if (nonReadOnly) readOnlySnapshot().iterator
     else new CtrieIterator(this)
-  
+
+  override def toArray[A1 >: (K, V)](implicit evidence$1: ClassManifest[A1]): Array[A1] = toArray
+
+  override def toIndexedSeq[A1 >: (K, V)]: immutable.IndexedSeq[A1] = null
 }
 
 object ConcurrentTrie {
