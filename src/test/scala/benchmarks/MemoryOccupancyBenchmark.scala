42a3c136dacffc9a50e5c28ea54076dc8276b0e3
veredz <verose@live.com>
2017-08-23 22:50:47 +0300

    fix to test memory footprint, group tests together under one object and removed unnecessary tests

--- src/test/scala/benchmarks/MemoryOccupancyBenchmark.scala
+++ src/test/scala/benchmarks/MemoryOccupancyBenchmark.scala
@@ -5,9 +5,7 @@ import org.scalameter.api._
 import org.scalameter.picklers.noPickler._
 
 
-object CTrieMemoryOccupancyBenchmark extends Bench.OfflineReport {
-  import ctries2.ConcurrentTrie
-
+object MemoryOccupancyBenchmark extends Bench.OfflineReport {
   override def reporter: Reporter.Composite[Double] = Reporter.Composite(
     new RegressionReporter(
       RegressionReporter.Tester.Accepter(),
@@ -18,11 +16,15 @@ object CTrieMemoryOccupancyBenchmark extends Bench.OfflineReport {
   override def executor = SeparateJvmsExecutor(
     new Executor.Warmer.Default,
     Aggregator.average,
-    new Measurer.Default)
+    new Measurer.MemoryFootprint)
   override def persistor: Persistor.None.type = Persistor.None
+  override def measurer: Measurer.MemoryFootprint = new Executor.Measurer.MemoryFootprint
 
-  var ct = new ConcurrentTrie[Elem, Elem]
-  val runs: Gen[Int] = Gen.single("run")(rep)
+  var e: Array[Elem] = elems
+
+  val runs: Gen[Int] = Gen.range("runs")(0, 0, 1)
+  var ct: Gen[ctries2.ConcurrentTrie[Elem, Elem]] = for (_ <- runs) yield new ctries2.ConcurrentTrie[Elem, Elem]
+  var ctlock: Gen[ctrielock.ConcurrentTrie[Elem, Elem]] = for (_ <- runs) yield new ctrielock.ConcurrentTrie[Elem, Elem]
 
   override def defaultConfig: Context = Context(
     exec.minWarmupRuns -> minWarmupRuns,            // minimum num of warmups
@@ -33,104 +35,33 @@ object CTrieMemoryOccupancyBenchmark extends Bench.OfflineReport {
   )
 
   performance of "MemoryOccupancy" in {
-    measure method "AfterDelete" in {
-      using(runs) setUp { _ =>
-        ct = new ConcurrentTrie[Elem, Elem]
-      } beforeTests {
-        if(debug) println("Starting test AfterDelete")
-      } afterTests {
-        if(debug) println("Finished AfterDelete")
-      } in { _ =>
-        val e = elems
-
-        for (i <- 0 until sz) ct.update(e(i), e(i))
-        for (i <- 0 until sz) ct.remove(e(i))
-
-        Runtime.getRuntime.gc()
-        while (true) {}
-      }
-    }
-
     measure method "Memory" in {
-      using(runs) setUp { _ =>
-        ct = new ConcurrentTrie[Elem, Elem]
+      using(ct) setUp { _ =>
+        e = elems
+      } tearDown { _ =>
+        Runtime.getRuntime.gc()
       } beforeTests {
         if(debug) println("Starting test Memory")
       } afterTests {
         if(debug) println("Finished Memory")
-      } in { _ =>
-        val e = elems
-
+      } in { ct =>
         for (i <- 0 until sz) ct.update(e(i), e(i))
-
-        Runtime.getRuntime.gc()
-        while (true) {}
-      }
-    }
-  }
-}
-
-
-object CTrieLockMemoryOccupancyBenchmark extends Bench.OfflineReport {
-  import ctrielock.ConcurrentTrie
-
-  override def reporter: Reporter.Composite[Double] = Reporter.Composite(
-    new RegressionReporter(
-      RegressionReporter.Tester.Accepter(),
-      RegressionReporter.Historian.Window(1)),
-    HtmlReporter(embedDsv),
-    new LoggingReporter()
-  )
-  override def executor = SeparateJvmsExecutor(
-    new Executor.Warmer.Default,
-    Aggregator.average,
-    new Measurer.Default)
-  override def persistor: Persistor.None.type = Persistor.None
-
-  var ct = new ConcurrentTrie[Elem, Elem]
-  val runs: Gen[Int] = Gen.single("run")(rep)
-
-  override def defaultConfig: Context = Context(
-    exec.minWarmupRuns -> minWarmupRuns,            // minimum num of warmups
-    exec.benchRuns -> benchRuns,                    // desired num of measurements
-    exec.independentSamples -> independentSamples,  // number of JVM instances
-    exec.jvmflags -> jvmParams,                     // JVM params, used to limit reserved space (default: 2GB)
-    reports.resultDir -> "target/benchmarks"
-  )
-
-  performance of "MemoryOccupancy" in {
-    measure method "AfterDelete" in {
-      using(runs) setUp { _ =>
-        ct = new ConcurrentTrie[Elem, Elem]
-      } beforeTests {
-        if(debug) println("Starting test AfterDelete")
-      } afterTests {
-        if(debug) println("Finished AfterDelete")
-      } in { _ =>
-        val e = elems
-
-        for (i <- 0 until sz) ct.update(e(i), e(i))
-        for (i <- 0 until sz) ct.remove(e(i))
-
-        Runtime.getRuntime.gc()
-        while (true) {}
+        ct // scalatest measures the memory footprint of the object the return value of the benchmark snippet references.
       }
     }
 
-    measure method "Memory" in {
-      using(runs) setUp { _ =>
-        ct = new ConcurrentTrie[Elem, Elem]
+    measure method "Memory-Lock" in {
+      using(ctlock) setUp { _ =>
+        e = elems
+      } tearDown { _ =>
+        Runtime.getRuntime.gc()
       } beforeTests {
         if(debug) println("Starting test Memory")
       } afterTests {
         if(debug) println("Finished Memory")
-      } in { _ =>
-        val e = elems
-
-        for (i <- 0 until sz) ct.update(e(i), e(i))
-
-        Runtime.getRuntime.gc()
-        while (true) {}
+      } in { ctlock =>
+        for (i <- 0 until sz) ctlock.update(e(i), e(i))
+        ctlock // scalatest measures the memory footprint of the object the return value of the benchmark snippet references.
       }
     }
   }
